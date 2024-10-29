3c3a7962d51f49873efd9801d83ce5fcf734a12f
K2MnO4 <1048117272@qq.com>
2024-10-29 17:51:50 +0800

    Add: loading data in parallel

--- train.py
+++ train.py
@@ -9,34 +9,35 @@ from model import NetWork
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
-
 transform = transforms.ToTensor()
 
 train_data = load_dataset("ylecun/mnist")["train"]
 train_data = train_data.map(lambda example: {"image": transform(example["image"]), 
-                                             "label": torch.tensor(example["label"])})
+                                            "label": torch.tensor(example["label"])})
 train_data.set_format("torch") # data should be set torch again!
 
-train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+if __name__ == "__main__":
 
-model = NetWork()
-optimizer = optim.Adam(model.parameters(), lr=5e-3)
-criterion = nn.CrossEntropyLoss()
+    train_loader = DataLoader(train_data, batch_size=64, shuffle=True, num_workers=4)
 
-train_epoches = 2
-cnt = 0
-for epoch in range(train_epoches):
-    for batch_idx, item in enumerate(train_loader):
-        images, labels = item["image"], item["label"]
-        scores = model(images)
-        loss = criterion(scores, labels)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+    model = NetWork()
+    optimizer = optim.Adam(model.parameters(), lr=5e-3)
+    criterion = nn.CrossEntropyLoss()
 
-        if batch_idx % 100 == 0:
-            print(f"Epoch {epoch + 1}/{train_epoches} "
-                    f"| Batch {batch_idx}/{len(train_loader)} "
-                    f"| Loss: {loss.item():.4f}")
+    train_epoches = 2
 
-torch.save(model.state_dict(), "mnist.pth")
+    for epoch in range(train_epoches):
+        for batch_idx, item in enumerate(train_loader):
+            images, labels = item["image"], item["label"]
+            scores = model(images)
+            loss = criterion(scores, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if batch_idx % 100 == 0:
+                print(f"Epoch {epoch + 1}/{train_epoches} "
+                        f"| Batch {batch_idx}/{len(train_loader)} "
+                        f"| Loss: {loss.item():.4f}")
+
+    torch.save(model.state_dict(), "mnist.pth")
