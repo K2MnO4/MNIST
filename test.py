64265651a94671625416fa0055eda13a70d544ac
K2MnO4 <1048117272@qq.com>
2024-10-31 18:33:12 +0800

    Add: utilize cuda to train and evaluate the model; Ban to calculate gradient in evaulation

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from datasets import load_dataset
 
+import time
+
 import torch
 from torch import nn
 from torch import optim
@@ -18,15 +20,23 @@ test_data.set_format("torch") # data should be set torch again!
 
 model = NetWork()
 model.load_state_dict(torch.load('mnist.pth'))
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+model.to(device)
 
 cnt = 0 # True predicted labels
+st_time = time.time()
 
-for item in test_data:
-    image = item["image"]
-    label = item["label"]
-    output = model(image)
-    predict_label = output.argmax(-1).item()
-    if predict_label == label:
-        cnt += 1
+with torch.no_grad():
+    for item in test_data:
+        image = item["image"].to(device)
+        label = item["label"].to(device)
+        image.to(device)
+        label.to(device)
+        output = model(image)
+        predict_label = output.argmax(-1).item()
+        if predict_label == label:
+            cnt += 1
 accuracy = float(cnt) / float(len(test_data))
-print(f"Accuracy: {accuracy}")
+ed_time = time.time()
+print(f"Accuracy: {accuracy} Time: {ed_time - st_time}")
